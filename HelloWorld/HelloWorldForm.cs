e3ee287a55205b04f3b2315a2c16bbaafecec39c
René Willemsen <r.willemsen@construsteel.com>
2021-02-24 11:39:16 +0100

    Add project files.

---
+++ HelloWorld/HelloWorldForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using Tekla.Structures.Dialog;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldForm : PluginFormBase
+    {
+        public HelloWorldForm()
+        {
+            InitializeComponent();
+
+            MessageBox.Show("Hello Constructor");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+    }
+}
---
+++ HelloWorld/HelloWorldPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    [Tekla.Structures.Plugins.Plugin("HelloWorld")]
+    [Tekla.Structures.Plugins.PluginUserInterface("HelloWorld.HelloWorldForm")]
+    [Tekla.Structures.Plugins.InputObjectDependency(InputObjectDependency.NOT_DEPENDENT)]
+    public class HelloWorldPlugin : Tekla.Structures.Plugins.PluginBase
+    {
+        public override List<InputDefinition> DefineInput()
+        {
+            return null;
+        }
+
+        public override bool Run(List<InputDefinition> Input)
+        {
+            return false;
+        }
+    }
+}
